303f88a6803029eeeca8546728222c8bf8b96c5b
sofasofaaoa <ladyrobertus3@gmail.com>
2023-02-01 13:03:14 +0700

    01.02

--- js/main.js
+++ js/main.js
@@ -3,13 +3,24 @@ let eventBus = new Vue()
 Vue.component('cols', {
     template:`
     <div id="cols">
-          <newcard></newcard>
-          <div class="col">{{ column1 }}</div>
-          <div class="col">{{ column2 }}</div>
-          <div class="col">{{ column3 }}</div>
-          
+        <newcard></newcard>
+        <div class="col">
+            <ul>
+                <li class="cards" v-for="card in column1" ><p>{{ card.title }}</p>
+                    <div>
+                        <ul>
+                            <li class="tasks" v-for="t in card.subtasks" v-if="t.title != null">
+                                <input class="checkbox" type="checkbox">
+                                <p>{{t.title}}</p>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="col">{{ column2 }}</div>
+        <div class="col">{{ column3 }}</div>
     </div>
-    
 `,
     data() {
         return {
@@ -25,67 +36,85 @@ Vue.component('cols', {
         eventBus.$on('card-submitted', card => {
             this.column1.push(card)
         })
+    },
+    props: {
+        card: {
+            title: {
+                type: Text,
+                required: true
+            },
+            subtasks: {
+                type: Array,
+                required: true,
+            }
+        },
+
     }
 })
 
-Vue.component('column', {
-    template:`
-    <div class="col">
-        
-    </div>
-`,
-})
-
-Vue.component('cards', {
-    template: `
-    <div>
-        
-    </div>
-    `,
-
-})
 
 Vue.component('newcard', {
     template: `
     <form class="addform" @submit.prevent="onSubmit">
         <p>
             <label for="title">Title</label>
-            <input class="title" v-model="title" type="text" placeholder="title">
+            <input class="title" v-model="title" maxlength="30" type="text" placeholder="title">
         </p>
         <div>
             <input class="checkbox" type="checkbox">
-            <input class="subtask" v-model="subtasks" type="text" placeholder="subtask">
+            <input required id="subtask1" v-model="subtask1" maxlength="30" placeholder="subtask">
         </div>
         <div>
             <input class="checkbox" type="checkbox">
-            <input class="subtask" v-model="subtasks" type="text" placeholder="subtask">
+            <input required id="subtask2" v-model="subtask2" maxlength="30" placeholder="subtask">
         </div>
         <div>
             <input class="checkbox" type="checkbox">
-            <input class="subtask" v-model="subtasks" type="text" placeholder="subtask">
+            <input required id="subtask3" v-model="subtask3" maxlength="30" placeholder="subtask">
         </div>
-        <button class="addsub">Add a subtask</button>
+        <div>
+            <input class="checkbox" type="checkbox">
+            <input  id="subtask4" v-model="subtask4" maxlength="30" placeholder="subtask">
+        </div>
+        <div>
+            <input class="checkbox" type="checkbox">
+            <input  id="subtask5" v-model="subtask5" maxlength="30" placeholder="subtask">
+        </div>
         <button type="submit">Add a card</button>
     </form>
     `,
     data() {
         return {
             title: null,
-            subtasks: [],
+            subtask1: null,
+            subtask2: null,
+            subtask3: null,
+            subtask4: null,
+            subtask5: null,
         }
     },
     methods: {
         onSubmit() {
             let card = {
                 title: this.title,
-                subtasks: this.subtasks,
+                subtasks: [{title: this.subtask1, completed: false},
+                            {title: this.subtask2, completed: false},
+                            {title: this.subtask3, completed: false},
+                            {title: this.subtask4, completed: false},
+                            {title: this.subtask5, completed: false}]
             }
             eventBus.$emit('card-submitted', card)
             this.title = null
-            this.subtasks = null
+            this.subtask1 = null
+            this.subtask2 = null
+            this.subtask3 = null
+            this.subtask4 = null
+            this.subtask5 = null
+            console.log(card)
         }
     }
 })
+
 
 let app = new Vue({
     el: '#app',
